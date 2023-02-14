fd087d3f5502ab79955d7c8ed302ebc336931980
MrKraack <MrKraack@gmail.com>
2023-02-14 09:39:24 +0100

    Testing

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
+let mysql = require('mysql');
+
+//Elements from DOM
+let ulDom = document.getElementById("tasklistUL");
+
+const connection = mysql.createConnection({
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_DBNAME
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'TurTur-do' });
+});
+
+router.get('/getTasks', function(req,res, next){
+  connection.connect();
+  console.log("Fetching data...");
+  let queryTest = 'SELECT * FROM tasks'
+  connection.query(queryTest, (err, result) => {
+    console.log(result);
+    for(let i = 0; i < result.length; i++){
+      let liEl = document.createElement("li");
+      liEl.appendChild(document.createTextNode(result[0]));
+      ulDom.appendChild(liEl);
+
+    }
+  })
+  connection.end();
+  res.render('index')
 });
 
 module.exports = router;
