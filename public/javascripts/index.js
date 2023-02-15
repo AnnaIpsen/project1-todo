aae4898f3ea19fdf8e6dc315f0b741bac0f23e59
Anna-1902 <annaipsen19@gmail.com>
2023-02-15 13:48:13 +0100

    add list modal + eventlisteners on modals and buttons

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -3,7 +3,9 @@ document.getElementById('addNewTask').addEventListener('click',() => {
     modal.style.display = 'flex'
 })
 
-document.getElementById('newTaskModalWrapper').addEventListener('click',() => {
+document.getElementById('newTaskModalWrapper').addEventListener('click',function (e) {
+    if (e.target !== this)
+        return;
     let modal = document.getElementById('newTaskModalWrapper')
     modal.style.display = 'none'
 })
@@ -13,7 +15,55 @@ document.getElementById('openModal').addEventListener('click', () => {
     modal.style.display = 'flex'
 })
 
-document.getElementById('taskModalWrapper').addEventListener('click',() => {
+document.getElementById('taskModalWrapper').addEventListener('click',function(e){
+    if (e.target !== this)
+        return;
     let modal = document.getElementById('taskModalWrapper')
+    modal.style.display = 'none'
+})
+
+document.getElementById('openNewTaskStateDropdown').addEventListener('click', function (event) {
+    if (event.target !== this)
+        return;
+    let drop = document.getElementById('newTaskStateDropdown')
+    if (drop.style.display === 'none') {
+        drop.style.display = 'block'
+    }
+    else {
+        drop.style.display = 'none'
+    }
+})
+
+document.getElementById('openTaskStateDropdown').addEventListener('click', function (event) {
+    if (event.target !== this)
+        return;
+    let drop = document.getElementById('taskStateDropdown')
+    if (drop.style.display === 'none') {
+        drop.style.display = 'block'
+    }
+    else {
+        drop.style.display = 'none'
+    }
+})
+
+document.getElementById('openCardStateDropdown').addEventListener('click', function (event) {
+    event.stopPropagation()
+    let drop = document.getElementById('cardStateDropdown')
+    if (drop.style.display === 'none') {
+        drop.style.display = 'block'
+    }
+    else {
+        drop.style.display = 'none'
+    }
+})
+
+document.getElementById('addNewList').addEventListener('click', () => {
+    let modal = document.getElementById('newListModalWrapper')
+    modal.style.display = 'flex'
+})
+document.getElementById('newListModalWrapper').addEventListener('click',function(e){
+    if (e.target !== this)
+        return;
+    let modal = document.getElementById('newListModalWrapper')
     modal.style.display = 'none'
 })
