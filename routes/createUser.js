f04b1c715160ee7fdbe0c51a502b922d29f15466
caldecreativo <93321331+caldecreativo@users.noreply.github.com>
2023-02-15 19:57:30 +0100

    hashing & salting username, password for createUser

--- routes/createUser.js
+++ routes/createUser.js
@@ -7,22 +7,31 @@ router.get('/', function(req, res, next) {
 });
 
 const { body, validationResult } = require("express-validator");
-
+const crypto = require('crypto-js');
 
 router.post("/create", 
- body("email").isEmail().blacklist("'"),
- body("username").isLength({min: 6}).isAlphanumeric().blacklist("'").blacklist(" "), 
- body("password").blacklist("'").blacklist(" "),
+ body("email").isEmail().blacklist("'").blacklist(" ").blacklist("-"),
+ body("username").isLength({min: 6}).isAlphanumeric().blacklist("'").blacklist(" ").blacklist("-"), 
+ body("password").isLength({min: 6}).blacklist("'").blacklist(" ").blacklist("-"),
  (req, res) =>{
 
-  // body("username", "not valid").isLength({min: 6}).escape(),
-  // body("password", "not valid").isLength({min: 6}).escape()
   const errors = validationResult(req);
     if (!errors.isEmpty()) {
       console.log(errors);
       return res.status(400).json({ errors: errors.array() });
     }
-  res.render("index")
+
+    let username = req.body.username;
+    let password = req.body.password;
+
+    let salt = crypto.lib.WordArray.random(10).toString();
+
+    let hashedUsername = crypto.SHA256(username + salt).toString();
+    let hashedPassword = crypto.SHA256(password + salt).toString();
+
+    console.log('Hashed username:', hashedUsername);
+    console.log('Hashed password:', hashedPassword);
+  res.render("createUser")
 
 });
 
--- routes/login.js
+++ routes/login.js
@@ -6,4 +6,25 @@ router.get('/', function(req, res, next) {
     res.render('login');
 });
 
+// const { body, validationResult } = require("express-validator");
+// const crypto = require('crypto-js');
+
+// router.post("/login", 
+//  body("loginUsername").isLength({min: 6}).isAlphanumeric().blacklist("'").blacklist(" ").blacklist("-"), 
+//  body("loginPassword").isLength({min: 6}).blacklist("'").blacklist(" ").blacklist("-"),
+//  (req, res) =>{
+
+//   const errors = validationResult(req);
+//     if (!errors.isEmpty()) {
+//       console.log(errors);
+//       return res.status(400).json({ errors: errors.array() });
+//     }
+
+//     let loginUsername = req.body.loginUsername;
+//     let loginPassword = req.body.loginPassword;
+
+
+
+// });
+
 module.exports = router;
