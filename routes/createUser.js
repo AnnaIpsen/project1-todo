76ae764c6ef107122eb38cfd20d867e2e4222a8a
MrKraack <MrKraack@gmail.com>
2023-02-16 15:52:19 +0100

    MySQL is now hosted

--- routes/createUser.js
+++ routes/createUser.js
@@ -4,6 +4,7 @@ let mysql = require("mysql");
 
 const connection = mysql.createConnection({
     host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_DBNAME
